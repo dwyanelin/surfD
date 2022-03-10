4d0560de162749420d9fb427791956b8a46a7609
Dwyane Lin <dw3lin@gmail.com>
2022-03-10 19:33:03 +0800

    add linebot.

--- server.js
+++ server.js
@@ -6,6 +6,34 @@ const rp=require('request-promise');
 const $=require('cheerio');
 const url='https://www.ptt.cc/bbs/fastfood/M.1526277935.A.DA0.html';
 
+// 引用linebot SDK
+var linebot=require('linebot');
+
+// 用於辨識Line Channel的資訊
+var bot=linebot({
+	channelId:'1656957916',
+	channelSecret:'9ad6181f07656cecde7ed27546281b37',
+	channelAccessToken:'mRylXzlEWz1SF/zZi2j5S6xfDXFXq66FSAXVRUCti7ljR0Ofm2bg33/CGk5UfQN8AnUx7hFY/v04p3wsl09egyN5p4jlxikPkYq/kp5uAHwSzZ98NeE72fFA9oL3bxxDi/JgVwX09gClqzO86YT9OAdB04t89/1O/w1cDnyilFU='
+});
+
+// 當有人傳送訊息給Bot時
+bot.on('message', function (event) {
+	// event.message.text是使用者傳給bot的訊息
+	// 準備要回傳的內容
+	var replyMsg = `Hello你剛才說的是:${event.message.text}`;
+	// 透過event.reply(要回傳的訊息)方法將訊息回傳給使用者
+	event.reply(replyMsg).then(function (data) {
+		// 當訊息成功回傳後的處理
+	}).catch(function (error) {
+		// 當訊息回傳失敗後的處理
+	});
+});
+
+// Bot所監聽的webhook路徑與port
+bot.listen('/linewebhook', 3000, function () {
+	console.log('[BOT已準備就緒]');
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
