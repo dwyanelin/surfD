e75eced931a3c5e342f2679ed2eb20f650c13391
Dwyane Lin <dw3lin@gmail.com>
2022-04-18 16:08:19 +0800

    module forecast reply button template test.

--- modules/forecast.js
+++ modules/forecast.js
@@ -141,34 +141,32 @@ module.exports=async (keyword)=>{
 		//提供box button給使用者選系統，再根據回傳訊息傳截圖
 		return {
 			"type": "template",
-			"altText": "This is a buttons template",
+			"altText": "this is a image carousel template",
 			"template": {
-				"type": "buttons",
-				"thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-				"imageAspectRatio": "rectangle",
-				"imageSize": "cover",
-				"imageBackgroundColor": "#FFFFFF",
-				"title": "Menu",
-				"text": "Please select",
-				"defaultAction": {
-					"type": "uri",
-					"label": "View detail",
-					"uri": "http://example.com/page/123"
-				},
-				"actions": [{
-					"type": "postback",
-					"label": "Buy",
-					"data": "action=buy&itemid=123"
+				"type": "image_carousel",
+				"columns": [{
+					"imageUrl": "https://example.com/bot/images/item1.jpg",
+					"action": {
+						"type": "postback",
+						"label": "Buy",
+						"data": "action=buy&itemid=111"
+					}
 				},
 				{
-					"type": "postback",
-					"label": "Add to cart",
-					"data": "action=add&itemid=123"
+					"imageUrl": "https://example.com/bot/images/item2.jpg",
+					"action": {
+						"type": "message",
+						"label": "Yes",
+						"text": "yes"
+					}
 				},
 				{
-					"type": "uri",
-					"label": "View detail",
-					"uri": "http://example.com/page/123"
+					"imageUrl": "https://example.com/bot/images/item3.jpg",
+					"action": {
+						"type": "uri",
+						"label": "View detail",
+						"uri": "http://example.com/page/222"
+					}
 				}]
 			}
 		}
