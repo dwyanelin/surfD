c993e2420777006134c7d5cefd696b4e749cce15
Dwyane Lin <dw3lin@gmail.com>
2022-05-05 10:57:03 +0800

    richmenu add about message. module about: add function introduction.

--- server.js
+++ server.js
@@ -23,19 +23,19 @@ const client=new line.Client(config);
 //Step1: create your image for richmenu
 //Step2: create richmenu object
 //Step3-1: createRichMenu and get richMenuId
-const richmenu=require("./modules/richmenu");
+/*const richmenu=require("./modules/richmenu");
 client.createRichMenu(richmenu)
 .then(richMenuId=>{
 	console.log("richMenuId: "+richMenuId)
 })
-.catch(err=>console.error(err));
+.catch(err=>console.error(err));*/
 
-//const richMenuId='richmenu-e1518bcf1598cfeb7429a513692d6b7b';
+const richMenuId='richmenu-a0baad6e6f8827b6d3b7a67e9959a6d9';
 
 //Step3-2: setRichMenuImage: upload image for RichMenu
-/*const fs=require('fs');
+const fs=require('fs');
 client.setRichMenuImage(richMenuId, fs.createReadStream('./images/richmenu.jpg'))
-.then(res=>console.log(res));*/
+.then(res=>console.log(res));
 
 //Step4: display to "all" users
 //client.setDefaultRichMenu(richMenuId);
