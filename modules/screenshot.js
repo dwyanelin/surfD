32c1bd719568c3fa50555e32a4ced4f1cfec45f0
Dwyane Lin <dw3lin@gmail.com>
2022-05-16 17:07:07 +0800

    windy test close particles animation.

--- modules/screenshot.js
+++ modules/screenshot.js
@@ -172,6 +172,11 @@ module.exports=async (url, viewport, system, browser, location)=>{
 			return [imageBuffer1, imageBuffer2, imageBuffer3];
 		}
 		else{
+			await page.goto("https://windy.com", {"waitUntil" : "networkidle0"});
+			const particles=await page.$('[id="particles"]');
+			console.log(particles);
+			const asdf=await page.$('[data-do="set,waves"]');
+			console.log(asdf);
 			await page.goto(url, {"waitUntil" : "networkidle0"});
 			const button1=await page.$('[data-do="set,waves"]');
 			await button1.evaluate(b=>b.click());
