b89bc6c74664493aaa82de14fab0d9c1ffb2be90
Dwyane Lin <dw3lin@gmail.com>
2022-05-16 18:20:28 +0800

    windy test close particles animation.

--- modules/screenshot.js
+++ modules/screenshot.js
@@ -189,20 +189,23 @@ module.exports=async (url, viewport, system, browser, location)=>{
 		else{
 			//粒子動畫關掉，增加執行速度
 			await page.goto("https://www.windy.com", {"waitUntil" : "networkidle0"});
-			/*const menu=await page.$('[data-do="rqstOpen,menu"]');
+			const menu=await page.$('[data-do="rqstOpen,menu"]');
+			console.log(menu);
 			if(menu!==null){
 				await menu.evaluate(b=>b.click());
+				await page.waitForNavigation();
 			}
-			await page.waitForNavigation();*/
 			const particles=await page.$('[id="menu-check-particles"]');
 			console.log(particles);
-			const className=await particles.getProperty('className');
-			console.log(className);
 			////class沒off才按
-			if(particles!==null&&!className.includes("off")){
-				await particles.evaluate(b=>b.click());
+			if(particles!==null){
+				const className=await particles.getProperty('className');
+				console.log(className);
+				if(!className.includes("off")){
+					await particles.evaluate(b=>b.click());
+					await page.waitForNavigation();
+				}
 			}
-			await page.waitForNavigation();
 			//粒子動畫關掉，增加執行速度
 
 			await page.goto(url, {"waitUntil" : "networkidle0"});
