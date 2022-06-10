46c06aac63a346d9547fb4d4bafec7c1fecffee6
Dwyane Lin <dw3lin@gmail.com>
2022-06-10 15:52:24 +0800

    add businesInformation.

--- modules/business.js
+++ modules/business.js
@@ -19,6 +19,9 @@ module.exports=(keyword)=>{
 	else if(keyword.includes("吉鵝")||keyword.toLowerCase().includes("jill")){
 		return businessFlexMessage(businessInformation.吉鵝);
 	}
+	else if(keyword.includes("外澳飯")||keyword.toLowerCase().includes("waiao")){
+		return businessFlexMessage(businessInformation.外澳飯);
+	}
 	/*else if(keyword.includes("豆你丸")||keyword.includes("逗你丸")||keyword.includes("豆你玩")||keyword.includes("逗你玩")){
 		return businessFlexMessage(businessInformation.豆你丸);
 	}*/
